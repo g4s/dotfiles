e7577bca961dd5bb2bce08cfc93e552091055901
Gregor Segner <gregor.segner@gmail.com>
2024-04-19 10:22:18 +0200

    adding the auto-install of ellipsis and ellipsis-packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,16 @@
 #
 # central bash bootstrapping script
 
+# ensure ellipsis and ellipsis-packages are present on system
+# ellipsis is a small, but powerfull dotfile manager
+# https://github.com/ellipsis/ellipsis
+declare -a packages=()
+if [[ ! $(command -v ellipsis) ]]; then
+  curl ellipsis.sh | sh
+else
+  for pkg in "${packages[@]}"; do
+    ellipsis install ${pkg}
+  done
+fi
+
 source ./commands.sh
