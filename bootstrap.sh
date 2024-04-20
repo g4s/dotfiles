b457d7b817030ade1bdbdce72caf2d597e6e73ce
Gregor Segner <gregor.segner@gmail.com>
2024-04-20 16:09:44 +0200

    rewriting code base for ellipsis installing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,71 @@
 #! /bin/bash
 #
-# central bash bootstrapping script
+# @author: Gregor A. Segner
+# @brief: central bootstrapping script for dotfiles
+#
+# This script uses the "Google Shell Style Guide"
+# --> https://google.github.io/styleguide/shellguide.html
+
 
 declare -a packages=()
 
-function initateSystem() {
-  # ensure ellipsis and ellipsis-packages are present on system
-  # ellipsis is a small, but powerfull dotfile manager
-  # https://github.com/ellipsis/ellipsis
-  if [[ ! $(command -v ellipsis) ]]; then
-    curl ellipsis.sh | sh
-    export PATH=~/.ellipsis/bin:$PATH
-  else
-    if [[ ! $(ls -A ./ellipsis/packages) ]]; then
-      for pkg in "${packages[@]}"; do
-        if [[ ${pkg} == "*/*" ]]; then
-          pkg = $( echo ${pkg} | cut -d "/" -f 2)
-        fi
-        if [[ ! -d "./ellipsis/packages/${pkg}" ]]; then
-          echo "installing ellipsis package ${pkg}"
-          ellipsis install ${pkg}
-        fi
-      done
+
+#######################################
+# This will install the ellipsis package manager and will also ask, if the 
+# usser will install additional packages. If the user decide to install
+# packages, the function will assemble the list of packages and will install
+# them.
+#
+# For more information about ellipsis, please visit the project git-repo
+# --> https://github.com/ellipsis/ellipsis
+#
+# GLOBALS
+#   packages
+#
+# ARGUMENTS
+#   None
+#######################################
+function installEllipsis() {
+  curl ellipsis.sh | sh
+  export PATH=~/.ellipsis/bin:$PATH
+
+  # the following block will configure ellipsis packages by
+  # modifying $packages
+  echo "Do you want select ellipsis-packages?"
+  select uchoice in "Yes" "No"; do
+    case $uchoice in
+      Yes ) 
+        echo "Do you want install g4s/ellipsis-git package?"
+        select uchoice in "Yes" "No"; do
+          case $uchoice in
+            Yes ) packages+=('g4s/ellipsis-git'); break;;
+            No ) break;;
+        break;;
+      No ) break;;
+    esac
+  done
+
+  # installing configured packages
+  for pkg in "${packages[@]}"; do
+    if [[ "${pkg}" == "*/*" ]]; then
+      pkg=$(echo ${pkg} | cut -d "/" -f 2)
     fi
-  fi
+    ellipsis install "${pkg}"
+  done
 }
 
+function initateSystem() {
+  echo "welcome to DIS - dotfile initiate system"
+  echo ""
+  echo "Do you want to install ellipsis dotfile manager?"
+  select uchoice in "Yes" "No"; do
+    case $uchoice in
+      Yes ) installEllipsis; break;;
+      No ) break;;
+    esac
+  done
+}
+
+### ENTRYPOINT
+initateSystem
 source ./commands.sh
